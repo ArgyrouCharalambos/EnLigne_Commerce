9bcb64f27fb853b0495b720bbefb15e38b6e389a
Argyrou <argykaselve@gmail.com>
2025-05-12 10:28:12 +0200

    ajout de la migration

--- database/migrations/1747036273174_create_products_table.ts
+++ database/migrations/1747036273174_create_products_table.ts
@@ -1,18 +1,23 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+// database/migrations/xxx_create_products.ts
+import {BaseSchema} from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'products'
 
-  async up() {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.string('name').notNullable()
+      table.text('description').nullable()
+      table.decimal('price', 8, 2).notNullable()
+      table.string('image').nullable()
+      table.string('thumbnail').nullable()
+      table.enum('category', ['electronics', 'fashion', 'home', 'sports'])
+      table.boolean('is_featured').defaultTo(false)
+      table.integer('stock').unsigned().defaultTo(0)
+      
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
     })
-  }
-
-  async down() {
-    this.schema.dropTable(this.tableName)
   }
 }
