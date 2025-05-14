6d15abcc2ae6b92dc4a175da61a2a6dcdc91d1e2
Argyrou <argykaselve@gmail.com>
2025-05-14 10:22:01 +0200

    modification du controller

--- start/routes.ts
+++ start/routes.ts
@@ -8,35 +8,10 @@
 */
 
 import router from '@adonisjs/core/services/router'
-const ShopController = () => import('#controllers/shops_controller')
-const CartController = () => import('#controllers/carts_controller')
-const CheckoutController = () => import('#controllers/checkouts_controller')
-const StaticController = () => import('#controllers/statics_controller')
+const ProductsController = () => import('#controllers/products_controller')
 
+router.get('/', async ({ view }) => {
+  return view.render('welcome')
+})
 
-// router.on('/').render('pages/home')
-
-// router.get('/', [ShopsController , 'index']).as('home')
-// router.get('/products/:id', [ShopsController , 'show']).as('product.show')
-
-// Catalogue
-router.get('/', [ShopController , 'index']).as('home')
-router.get('/produits/:slug', [ShopController , 'show']).as('products.show')
-router.get('/recherche', [ShopController , 'search']).as('products.search')
-
-// Panier
-router.get('/panier', [CartController , 'index']).as('cart.index')
-// router.post('/panier', [CartController , 'show']).as('cart.store')
-
-// Commande
-router.group(() => {
-    router.get('/', [CheckoutController , 'index']).as('index')
-    router.post('/process', [CheckoutController , 'process']).as('process')
-}).prefix('/checkout')
-
-// Pages statiques
-router.get('/a-propos', [StaticController , 'about']).as('about')
-router.get('/contact', [StaticController , 'contact']).as('contact')
-// // Pages statiques
-// router.get('/about', async ({ view }) => view.render('pages/about'))
-// router.get('/contact', async ({ view }) => view.render('pages/contact'))
+router.resource('products', ProductsController).except(['destroy'])
